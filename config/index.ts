5afcdc627333c99d880e5c3ef7b2c5ef1297d961
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2025-11-23 16:07:47 +0530

    updated conf

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,4 @@
 export const conf = {
-	BASE_URL: "http://localhost:8000",
-	// BASE_URL: "https://6ljvfygiw7lc4qbgpipxczo3fi0zqeug.lambda-url.us-east-1.on.aws",
+	// BASE_URL: "http://localhost:8000",
+	BASE_URL: "https://6ljvfygiw7lc4qbgpipxczo3fi0zqeug.lambda-url.us-east-1.on.aws",
 };
