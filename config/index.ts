12e7c2d90282ea13cabab7f9d41f125e286927ea
Rishabh Singh <rishabhsingh2305@gmail.com>
2025-12-28 23:42:37 +0530

    added new conf

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 export const conf = {
     // BASE_URL: "http://localhost:8000",
     BASE_URL:
-        "http://localhost:8002",
+        "http://localhost:2000/account",
     CDN_BASE_URL: "https://poc.r2s.space",
-    STREAM_BASE_URL: "http://localhost:8000",
+    STREAM_BASE_URL: "http://localhost:2000/stream",
 };
