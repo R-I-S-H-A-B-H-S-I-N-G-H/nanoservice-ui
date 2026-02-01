16dd8a852fe3048a6da2d3c3256a103298f28aa9
Rishabh Singh <rishabhsingh2305@gmail.com>
2026-02-01 22:57:09 +0530

    fixed conf

--- config/index.ts
+++ config/index.ts
@@ -6,7 +6,7 @@ export const conf = {
     // STREAM_BASE_URL: "https://stream.r2s.space/stream",
 
     BASE_URL:
-        "http://localhost:2000",
+        "https://stream.r2s.space",
     CDN_BASE_URL: "https://poc.r2s.space",
     STREAM_BASE_URL: "https://stream.r2s.space/stream",
     ASSET_CDN_URL: "https://assets-dev.b-cdn.net",
