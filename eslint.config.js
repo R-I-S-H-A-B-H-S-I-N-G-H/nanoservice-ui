39854ae9e9e860d0499aa17ca6ced088257bc614
Rishabh Singh <rishabhsingh2305@gmail.com>
2026-02-14 12:49:12 +0530

    Refactor ESLint configuration structure

--- eslint.config.js
+++ eslint.config.js
@@ -6,25 +6,18 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      // This turns the "error" into a "warning" so the build doesn't fail
-      '@typescript-eslint/no-unused-vars': 'warn', 
-      
-      // Recommended: ignore vars starting with an underscore (e.g., _data)
-      // '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
-    },
-  },
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
 ])
