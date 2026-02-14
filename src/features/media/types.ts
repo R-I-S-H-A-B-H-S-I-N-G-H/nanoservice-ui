e9ef38577852e39f8130281107c377cc99c00ec8
Rishabh Singh <rishabhsingh2305@gmail.com>
2026-02-14 13:06:53 +0530

    Enhance Media and Quota interfaces with additional fields
    Added optional fields to Media interface and modified Quota interface.

--- src/features/media/types.ts
+++ src/features/media/types.ts
@@ -1,11 +1,51 @@
 export interface Media {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  is_deleted: boolean;
-  name: string;
-  user_id: string;
-  org_id: string;
-  media_type: string;
-  url: string;
+	id?: string;
+	shortId?: string;
+	createdAt?: string;
+	updatedAt?: string;
+	isDeleted?: boolean;
+	orgId?: string;
+	userId?: string;
+	name: string;
+	description?: string;
+	mimeType?: string;
+	extension?: string;
+	resourcePath?: string;
+	size?: number;
+	status?: string;
+	preSignedUrl?: string;
 }
+
+export interface Quota {
+  id: string;
+  resourceKey: "storage_bytes" | "bandwidth_bytes";
+  limit: number;
+  currentUsage: number;
+}
+
+export interface QuotaResponse {
+  success: boolean;
+  data: Quota[];
+}
+
+
+
+/*
+{
+                "id": "715b863c-8e08-4503-b735-c7d65e2c2ed1",
+                "shortId": "GDwh8B6X",
+                "createdAt": "2026-01-26T00:43:08.926616+05:30",
+                "updatedAt": "2026-01-26T00:43:08.926616+05:30",
+                "isDeleted": false,
+                "orgId": "Jtdc6iYH",
+                "userId": "N-9vzsXH",
+                "name": "Test Stream 2",
+                "description": "Sintel",
+                "mimeType": "",
+                "extension": "",
+                "resourcePath": "Jtdc6iYH/N-9vzsXH/GDwh8B6X.",
+                "size": 0,
+                "status": "active",
+                "preSignedUrl": null
+            },
+			*/
